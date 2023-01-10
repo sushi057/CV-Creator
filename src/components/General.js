ade4fba40101c5431311deac1251751952878aa3
suvash <suvash2077@gmail.com>
2023-01-10 19:55:19 +0545

    Structuring Code

---
+++ src/components/General.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class General extends Component {
+  render() {
+    return (
+        <ul>
+            
+        </ul>
+    )
+  }
+}
---
+++ src/components/Preview.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class Preview extends Component {
+  render() {
+    return (
+      <>
+        <div className="general">
+          <h2>John Doe</h2>
+          <h4>Email:</h4>
+          <h4>Phone:</h4>
+          <h4>Hobbies</h4>
+          <ul>
+            <li>Books</li>
+            <li>Music</li>
+            <li>Gaming</li>
+            <li>Travel</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}

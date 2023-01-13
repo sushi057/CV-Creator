292b2bf855c016e108ca840224956066453e3354
suvash <suvash2077@gmail.com>
2023-01-13 20:30:24 +0545

    Basic Skeleton Completed

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -15,8 +15,26 @@ export default class Preview extends Component {
 
         <div className="main-section">
           <div className="summary">{this.props.summary}</div>
-          <div className="education"></div>
-          <div className="experience">exp</div>
+          <div className="education">
+            <div className="education-years">
+              {this.props.edu_startYear} - {this.props.edu_endYear}
+            </div>
+            <div className="college-level">
+              {this.props.college}
+              <br />
+              {this.props.level}
+            </div>
+          </div>
+          <div className="experience">
+            <div className="experience-years">
+              {this.props.exp_startYear} - {this.props.exp_lastYear}
+            </div>
+            <div className="company-position">
+              {this.props.company}
+              <br />
+              {this.props.position}
+            </div>
+          </div>
         </div>
       </>
     );
